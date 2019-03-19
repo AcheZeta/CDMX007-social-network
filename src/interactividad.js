f7f59543a645e42810d8fc33967f89d1feff5a3d
AcheZeta <ache.mariana@gmail.com>
2019-03-19 11:43:10 -0600

    Borrando para hacer fetch

--- src/interactividad.js
+++ src/interactividad.js
@@ -1,9 +1,3 @@
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.modal');
-        var instances = M.Modal.init(elems, options);
-      });
-    
