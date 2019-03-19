1961fcfe2ae4b9494b887dadc25b655472558258
AcheZeta <ache.mariana@gmail.com>
2019-03-19 10:59:54 -0600

    Antes de merge

--- src/interactividad.js
+++ src/interactividad.js
@@ -1,3 +1,9 @@
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems, options);
+      });
+    
